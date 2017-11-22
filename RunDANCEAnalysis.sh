20c77617f65181b0d595db8e190669948a2a547c
Christopher J. Prokop <cprokop@dance-cruncher3.lanl.gov>
2017-11-22 14:49:59 -0700

    Altered the TOF offsets for the beam monitors based on a rough analysis of the beam monitor data from the thick Co target.  Also fixed a bug in the En calculation in the beam monitors (was fixed before running Co analysis for beam monitors). Made the RunDANCEAnalysis script better and replaced its old functionality with the SimpleAnalysis script.

--- RunDANCEAnalysis.sh
+++ RunDANCEAnalysis.sh
@@ -1,27 +1,111 @@
 #!/bin/bash
 
-PATHTODATA=MIDAS_Data
-#PATHTODATA=MIDAS_Data_dancedaq_6
+source ~/.bashrc
 
-#These all live on data/14/
-for i in `seq 64693 64833`;  #Cu63
+(
+# Uncomment the following line for debugging output
+#set -x
 
-#for i in `seq 63015 63589`;  #Cu65
+/bin/date +%T;
 
-#for i in `seq 64989 65265`;  #Cu65
-#for i in `seq 65047 65265`;  #Cu65
+umask -S
+umask 022
+umask -S
 
-#for i in `seq 64137 64692`;  #Cu65
+#Cu63
+#FIRST_RUN=64693
+#LAST_RUN=64833
 
-#for i in `seq 64834 64987`;  #Pb208
+#Cu65
+#FIRST_RUN=63015
+#LAST_RUN=63589
 
+#Pb208
+#FIRST_RUN=60622
+#LAST_RUN=60811
 
+#Au197
+#FIRST_RUN=60409
+#LAST_RUN=60451
 
-#for i in `seq 67339 67357`;  #Co59 141.0mg
-#for i in `seq 67366 67401`;  #Co59 329.4mg
+#Au197
+#FIRST_RUN=60572
+#LAST_RUN=60584
 
+#Au197
+#FIRST_RUN=63590
+#LAST_RUN=63649
 
-do
-    echo Processing Run: $i;
-    ./DANCE_Analysis $PATHTODATA $i stage0.cfg;
+#Au197
+#FIRST_RUN=60409
+#LAST_RUN=60451
+
+DATA_DIR=/mnt/hygelac-data/14/dance/caen2015/
+ANALYSIS_DIR=/home/cprokop/CJP/DANCE_Analysis/
+STAGE0_DIR=$ANALYSIS_DIR/stage0_root/
+STAGE1_DIR=$ANALYSIS_DIR/stage1_root/
+ALPHA_CALIB_DIR=/home/cprokop/CJP/DANCE_Alpha_Calibrator
+
+for FILE in $DATA_DIR/run*.mid.gz ; do
+    
+    if [ -s $FILE ] ; then 
+	GZ_FILE=`basename $FILE`
+	BASENAME=${GZ_FILE%.mid.gz}
+	RUN=${BASENAME#run}
+	
+        # This is weird--to deal with the fact that there are leading zeros,
+        #   we pretend the numbers are in hex so that bash can do a numeric
+        #   comparison
+	HEXRUN=0x$RUN
+	HEXFIRST_RUN=0x$FIRST_RUN
+	HEXLAST_RUN=0x$LAST_RUN
+	if (( $HEXRUN > $HEXLAST_RUN )) ; then
+	    echo "Last run " $LAST_RUN "completed"
+	    exit 0
+	fi
+
+	if (( $HEXRUN >= $HEXFIRST_RUN )) ; then 
+	    #check for existing rootfile
+	    if [ ! -f $STAGE0_DIR/Stage0_Histograms_Run_${RUN#0}.root ] ; then
+		#switch to the analysis directory 
+		cd $ANALYSIS_DIR
+		
+		#Check to see if fare is built and then run the analysis
+		if [ -f DANCE_Analysis ] ; then
+		    echo "about to run stage 0 analyis"
+		./DANCE_Analysis $DATA_DIR $RUN stage0.cfg
+
+		fi		
+	    fi
+	    
+	    #switch to alpha calibrations
+	    if [ ! -f $ALPHA_CALIB_DIR/ParamOutput/param_out_${RUN#0}.txt ] ; then
+		cd $ALPHA_CALIB_DIR
+		if [ -f DANCE_Alpha_Calibrator ] ; then
+		    if [ -f $STAGE0_DIR/Stage0_Histograms_Run_${RUN#0}.root ] ; then
+			echo "about to run calibrations"
+			./DANCE_Alpha_Calibrator $RUN
+			cp $ALPHA_CALIB_DIR/ParamOutput/param_out_${RUN#0}.txt $ANALYSIS_DIR/Calibrations/.
+		    fi
+		fi
+	    fi   
+
+	    #check for existing rootfile
+	    if [ ! -f $STAGE1_DIR/Stage1_Histograms_Run_${RUN#0}.root ] ; then
+		#switch to the analysis directory 
+		cd $ANALYSIS_DIR
+		
+		#Check to see if fare is built and then run the analysis
+		if [ -f DANCE_Analysis ] ; then
+		    echo "about to run stage 1 analyis"
+		./DANCE_Analysis $DATA_DIR $RUN stage1.cfg
+
+		fi		
+	    fi
+	fi
+    fi
 done
+
+) | tee -a /tmp/`basename $0`.`date +%F`.log
+
+
