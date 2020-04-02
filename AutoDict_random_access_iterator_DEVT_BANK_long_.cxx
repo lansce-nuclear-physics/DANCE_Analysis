b97583b8ec6683d0c2ca6f67fb2d92b65fa2bb8b
Aaron Couture <acouture@acouture.lanl.gov>
2020-04-02 11:19:32 -0600

    cleanup to prepare to github version.

--- AutoDict_random_access_iterator_DEVT_BANK_long_.cxx
+++
@@ -1,9 +0,0 @@
-#include "analyzer.h"
-#ifdef __CINT__ 
-#pragma link off all class;
-#pragma link off all function;
-#pragma link off all global;
-#pragma link off all typedef;
-#pragma link C++ nestedclasses;
-#pragma link C++ class random_access_iterator<DEVT_BANK,long>+;
-#endif
--- AutoDict_vector_DEVT_BANK_.cxx
+++
@@ -1,9 +0,0 @@
-#include "analyzer.h"
-#ifdef __CINT__ 
-#pragma link off all class;
-#pragma link off all function;
-#pragma link off all global;
-#pragma link off all typedef;
-#pragma link C++ nestedclasses;
-#pragma link C++ class vector<DEVT_BANK>+;
-#endif
