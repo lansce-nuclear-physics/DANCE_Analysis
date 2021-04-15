651c77d3d77241a41b102c315029adb4dff31729
Cathleen Elizabeth Fry <cfry@dance-cruncher3.lanl.gov>
2021-04-15 14:19:31 -0600

    cleaning up a little nonsense

--- gates2018.sh
+++ gates2018.sh
@@ -5,4 +5,6 @@ unlink Gamma.dat
 ln -s Gamma_109430.dat Gamma.dat
 unlink Retrigger.dat
 ln -s Retrigger_105138.dat Retrigger.dat
+unlink Pileup.dat
+ln -s Pileup2018.dat Pileup.dat
 cd ..
--- gates2019.sh
+++ gates2019.sh
@@ -5,5 +5,7 @@ unlink Gamma.dat
 ln -s Gamma_113158.dat Gamma.dat
 unlink Retrigger.dat
 ln -s Retrigger_113341.dat Retrigger.dat
+unlink Pileup.dat
+ln -s Pileup2019.dat
 #ln -s Retrigger_113210.dat Retrigger.dat
 cd ..
